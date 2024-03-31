42d1457c665f9d05d6ec67ecded82aa243a3b2d4
ZamZamGraphics <zamzamprintmedia@gmail.com>
2024-03-31 14:36:36 +0600

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="antialiased text-slate-700 dark:text-slate-400 bg-gray-100 dark:bg-slate-800">
       <Sidebar />
       <Headers />
-      <div className="max-w-lg mx-auto mt-36 space-y-5">
+      <div className="ml-0 md:ml-[15rem] p-5 space-y-5">
         <div className="space-y-4">
           <Alerts variant="soft">
             alert! You should check in on some of those fields below.
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,55 @@
-import { HiBars3 } from "react-icons/hi2";
+import { BsEnvelope } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
+import { HiBars3, HiOutlineBellAlert } from "react-icons/hi2";
+import { FaPlus } from "react-icons/fa6";
+
 import TextField from "./TextField";
 
 function Header() {
   return (
     <div className="ml-0 md:ml-[15rem] bg-white border-b border-gray-200 sticky top-0 z-40">
-      <div className="flex items-center justify-between p-2">
-        <div className="flex items-center flex-shrink-0 space-x-5">
+      <div className="flex flex-row items-center justify-between p-2">
+        <div className="flex flex-auto items-center space-x-5">
           <div>
-            <button className="flex items-center p-1 text-3xl hover:bg-gray-100 rounded-md">
+            <button className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full">
               <span>
                 <HiBars3 />
               </span>
             </button>
           </div>
-          <div className="flex items-center space-x-3">
-            <TextField
-              type="text"
-              label="Search"
-              defaultValue="Search Anything ..."
-            />
+          <div className="w-full md:w-96">
+            <TextField type="text" defaultValue="Search Anything ..." />
           </div>
-          <div>Add New</div>
+          <div>
+            <button className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full">
+              <span>
+                <FaPlus />
+              </span>
+            </button>
+          </div>
         </div>
-        <div className="flex items-center ms-auto">
-          <div>Notification</div>
-          <div>Massage</div>
-          <div>User Profile</div>
+        <div className="flex ms-auto">
+          <div>
+            <button className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full">
+              <span>
+                <HiOutlineBellAlert />
+              </span>
+            </button>
+          </div>
+          <div>
+            <button className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full">
+              <span>
+                <BsEnvelope />
+              </span>
+            </button>
+          </div>
+          <div>
+            <button className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full">
+              <span>
+                <FaRegUserCircle />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
