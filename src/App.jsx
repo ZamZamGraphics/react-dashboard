acf677453cae52e54257f4b1375e202548f07929
ZamZamGraphics <zamzamprintmedia@gmail.com>
2024-04-01 11:57:34 +0600

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Alerts from "./components/Alerts";
 import Headers from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   const [open, setOpen] = useState(false);
+  const buttonRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (buttonRef.current && !buttonRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="antialiased text-slate-700 dark:text-slate-400 bg-gray-100 dark:bg-slate-800">
@@ -12,7 +27,7 @@ function App() {
         <div className="w-full h-screen backdrop-blur-sm bg-black/50 fixed bottom-0 z-50"></div>
       )}
       <Sidebar open={open} />
-      <Headers onOpen={() => setOpen(!open)} />
+      <Headers onOpen={() => setOpen(!open)} ref={buttonRef} />
       <div className="ml-0 md:ml-[15rem] p-5 space-y-5">
         <div className="space-y-4">
           <Alerts variant="soft">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
+import { forwardRef } from "react";
 import { BsEnvelope } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa6";
 import { HiBars3, HiOutlineBellAlert } from "react-icons/hi2";
-
 import TextField from "./TextField";
 
-function Header({ onOpen }) {
+function Header({ onOpen }, buttonRef) {
   return (
     <div className="ml-0 md:ml-[15rem] bg-white border-b border-gray-200 sticky top-0 z-40">
       <div className="flex items-center justify-between p-2">
         <div className="flex flex-auto items-center space-x-5">
           <div>
             <button
+              ref={buttonRef}
               onClick={onOpen}
               className="flex items-center p-2 text-2xl hover:bg-gray-100 rounded-full"
             >
@@ -59,4 +60,5 @@ function Header({ onOpen }) {
   );
 }
 
-export default Header;
+const forwardedHeader = forwardRef(Header);
+export default forwardedHeader;
